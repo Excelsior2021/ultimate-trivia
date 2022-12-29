3525c473f217b659b862a365c0b1244aceb1d2f7
Jonathan Kila <temidee11@yahoo.co.uk>
2022-12-29 18:44:14 +0000

    updates

--- development_scripts/script.ts
+++ development_scripts/script.ts
@@ -1,194 +1,194 @@
-import axios from "axios";
+import axios from "axios"
 
-const categoriesEl = document.getElementById("categories")!;
-const form = document.getElementById("form") as HTMLFormElement;
-const resultsEl = document.getElementById("results")!;
-const gameEl = document.getElementById("game")!;
-const resetEl = document.getElementById("game-reset")!;
-const messageEl = document.getElementById("message")!;
+const categoriesEl = document.getElementById("categories")!
+const form = document.getElementById("form") as HTMLFormElement
+const resultsEl = document.getElementById("results")!
+const gameEl = document.getElementById("game")!
+const resetEl = document.getElementById("game-reset")!
+const messageEl = document.getElementById("message")!
 
-let correct = 0;
-let incorrect = 0;
-let questionCounter = 0;
-let questionAmount: number;
-let questionIndex = 0;
+let correct = 0
+let incorrect = 0
+let questionCounter = 0
+let questionAmount: number
+let questionIndex = 0
 
 type categoryObj = {
-  id: number;
-  name: string;
-};
+  id: number
+  name: string
+}
 
 type resultsObj = {
-  category: string;
-  correct_answer: string;
-  difficulty: string;
-  incorrect_answers: string[];
-  question: string;
-  type: string;
-};
+  category: string
+  correct_answer: string
+  difficulty: string
+  incorrect_answers: string[]
+  question: string
+  type: string
+}
 
 const getCategories = async () => {
   try {
-    const { data } = await axios.get("https://opentdb.com/api_category.php");
+    const { data } = await axios.get("https://opentdb.com/api_category.php")
 
-    const categories: categoryObj[] = data.trivia_categories;
+    const categories: categoryObj[] = data.trivia_categories
 
     categories.map((category, value) => {
-      value += 9;
-      const optionEl = document.createElement("option");
-      optionEl.value = value.toString();
-      optionEl.innerText = category.name;
-      categoriesEl.appendChild(optionEl);
-    });
+      value += 9
+      const optionEl = document.createElement("option")
+      optionEl.value = value.toString()
+      optionEl.innerText = category.name
+      categoriesEl.appendChild(optionEl)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 form.addEventListener("submit", event => {
-  event.preventDefault();
+  event.preventDefault()
 
-  gameEl.innerHTML = "";
-  resultsEl.innerHTML = "";
-  correct = 0;
-  incorrect = 0;
-  questionCounter = 0;
-  questionIndex = 0;
+  gameEl.innerHTML = ""
+  resultsEl.innerHTML = ""
+  correct = 0
+  incorrect = 0
+  questionCounter = 0
+  questionIndex = 0
 
-  getQuestions();
-});
+  getQuestions()
+})
 
 const getQuestions = async () => {
-  const amount = form.questions_num.value;
-  questionAmount = parseInt(amount);
-  const category = form.categories.value;
-  const difficulty = form.difficulty.value;
-  const type = form.question_type.value;
+  const amount = form.questions_num.value
+  questionAmount = parseInt(amount)
+  const category = form.categories.value
+  const difficulty = form.difficulty.value
+  const type = form.question_type.value
 
   try {
     const { data } = await axios.get(
       `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`
-    );
+    )
 
-    form.classList.add("form--hidden");
-    gameEl.classList.remove("game--hidden");
-    gameEl.classList.add("game--format");
-    resultsEl.classList.add("results--hidden");
-    resetEl.classList.remove("game__reset--hidden");
-    messageEl.classList.add("message--hidden");
+    form.classList.add("form--hidden")
+    gameEl.classList.remove("game--hidden")
+    gameEl.classList.add("game--format")
+    resultsEl.classList.add("results--hidden")
+    resetEl.classList.remove("game__reset--hidden")
+    messageEl.classList.add("message--hidden")
 
-    data.response_code === 1 ? noData() : null;
+    data.response_code === 1 ? noData() : null
 
-    createQuestions(data.results);
+    createQuestions(data.results)
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 const noData = () => {
-  const message = document.createElement("p");
-  message.classList.add("no-data__message");
+  const message = document.createElement("p")
+  message.classList.add("no-data__message")
   message.innerText =
-    "There were no questions for your chosen settings. Try reducing the number of questions in the settings.";
+    "There were no questions for your chosen settings. Try reducing the number of questions in the settings."
 
-  const button = document.createElement("button");
-  button.innerText = "go back";
-  button.classList.add("no-data__button");
-  button.classList.add("game__navigate");
-  button.addEventListener("click", restart);
+  const button = document.createElement("button")
+  button.innerText = "go back"
+  button.classList.add("no-data__button")
+  button.classList.add("game__navigate")
+  button.addEventListener("click", restart)
 
-  gameEl.appendChild(message);
-  gameEl.appendChild(button);
-};
+  gameEl.appendChild(message)
+  gameEl.appendChild(button)
+}
 
 const createQuestions = (results: resultsObj[]) => {
   results.forEach((obj, i) => {
-    i += 1;
-    const answers: HTMLInputElement[] = [];
-    const questionDiv = document.createElement("div");
-    questionDiv.classList.add("game__questions");
+    i += 1
+    const answers: HTMLInputElement[] = []
+    const questionDiv = document.createElement("div")
+    questionDiv.classList.add("game__questions")
     if (i !== 1) {
-      questionDiv.classList.add("game__questions--hidden");
+      questionDiv.classList.add("game__questions--hidden")
     }
-    const questionTitle = document.createElement("h2");
-    questionTitle.classList.add("game__title");
-    questionTitle.innerText = `Question ${i}`;
-    const questionEl = document.createElement("p");
-    questionEl.classList.add("game__question");
-    const parsedQ = htmlEntities(obj.question);
-    questionEl.innerText = parsedQ;
+    const questionTitle = document.createElement("h2")
+    questionTitle.classList.add("game__title")
+    questionTitle.innerText = `Question ${i}`
+    const questionEl = document.createElement("p")
+    questionEl.classList.add("game__question")
+    const parsedQ = htmlEntities(obj.question)
+    questionEl.innerText = parsedQ
 
     obj.incorrect_answers.forEach(ans => {
-      const ansEl = document.createElement("input");
-      ansEl.type = "button";
-      ansEl.classList.add("game__answer");
-      const parsed = htmlEntities(ans);
-      ansEl.name = "false";
-      ansEl.value = parsed;
-      ansEl.innerText = parsed;
-      ansEl.addEventListener("click", event => checkAnswer(event, answers));
-      answers.push(ansEl);
-    });
+      const ansEl = document.createElement("input")
+      ansEl.type = "button"
+      ansEl.classList.add("game__answer")
+      const parsed = htmlEntities(ans)
+      ansEl.name = "false"
+      ansEl.value = parsed
+      ansEl.innerText = parsed
+      ansEl.addEventListener("click", event => checkAnswer(event, answers))
+      answers.push(ansEl)
+    })
 
-    const ansEl = document.createElement("input");
-    ansEl.type = "button";
-    ansEl.classList.add("game__answer");
-    const parsedA = htmlEntities(obj.correct_answer);
-    ansEl.name = "true";
-    ansEl.value = parsedA;
-    ansEl.addEventListener("click", event => checkAnswer(event, answers));
-    answers.push(ansEl);
+    const ansEl = document.createElement("input")
+    ansEl.type = "button"
+    ansEl.classList.add("game__answer")
+    const parsedA = htmlEntities(obj.correct_answer)
+    ansEl.name = "true"
+    ansEl.value = parsedA
+    ansEl.addEventListener("click", event => checkAnswer(event, answers))
+    answers.push(ansEl)
 
-    const shuffledAnswers = shuffleArray(answers);
-    questionDiv.appendChild(questionTitle);
-    questionDiv.appendChild(questionEl);
-    shuffledAnswers.forEach(ans => questionDiv.appendChild(ans));
+    const shuffledAnswers = shuffleArray(answers)
+    questionDiv.appendChild(questionTitle)
+    questionDiv.appendChild(questionEl)
+    shuffledAnswers.forEach(ans => questionDiv.appendChild(ans))
 
-    const buttonsDiv = document.createElement("div");
-    const buttonDiv = document.createElement("div");
-    const buttonDivTwo = document.createElement("div");
+    const buttonsDiv = document.createElement("div")
+    const buttonDiv = document.createElement("div")
+    const buttonDivTwo = document.createElement("div")
 
-    buttonsDiv.classList.add("game__navigate-container");
+    buttonsDiv.classList.add("game__navigate-container")
 
     if (i !== 1) {
-      const prevButton = document.createElement("input");
-      prevButton.classList.add("game__navigate");
-      prevButton.classList.add("game__navigate--prev");
-      prevButton.type = "button";
-      prevButton.value = "prev";
-      prevButton.addEventListener("click", prevQuestion);
-      buttonDiv.appendChild(prevButton);
+      const prevButton = document.createElement("input")
+      prevButton.classList.add("game__navigate")
+      prevButton.classList.add("game__navigate--prev")
+      prevButton.type = "button"
+      prevButton.value = "prev"
+      prevButton.addEventListener("click", prevQuestion)
+      buttonDiv.appendChild(prevButton)
 
       i === questionAmount
         ? prevButton.classList.add("game__navigate--prev--format")
-        : null;
+        : null
     }
 
     if (i !== questionAmount) {
-      const nextButton = document.createElement("input");
-      nextButton.classList.add("game__navigate");
-      nextButton.classList.add("game__navigate--next");
-      nextButton.type = "button";
-      nextButton.value = "next";
-      nextButton.addEventListener("click", nextQuestion);
-      buttonDiv.appendChild(nextButton);
+      const nextButton = document.createElement("input")
+      nextButton.classList.add("game__navigate")
+      nextButton.classList.add("game__navigate--next")
+      nextButton.type = "button"
+      nextButton.value = "next"
+      nextButton.addEventListener("click", nextQuestion)
+      buttonDiv.appendChild(nextButton)
     }
 
-    const viewResults = document.createElement("input");
-    viewResults.classList.add("game__navigate");
-    viewResults.classList.add("game__navigate--results");
-    viewResults.type = "button";
-    viewResults.value = "view results";
-    viewResults.addEventListener("click", showResults);
-    buttonDivTwo.appendChild(viewResults);
+    const viewResults = document.createElement("input")
+    viewResults.classList.add("game__navigate")
+    viewResults.classList.add("game__navigate--results")
+    viewResults.type = "button"
+    viewResults.value = "view results"
+    viewResults.addEventListener("click", showResults)
+    buttonDivTwo.appendChild(viewResults)
 
-    buttonsDiv.appendChild(buttonDiv);
-    buttonsDiv.appendChild(buttonDivTwo);
+    buttonsDiv.appendChild(buttonDiv)
+    buttonsDiv.appendChild(buttonDivTwo)
 
-    questionDiv.appendChild(buttonsDiv);
-    gameEl.appendChild(questionDiv);
-  });
-};
+    questionDiv.appendChild(buttonsDiv)
+    gameEl.appendChild(questionDiv)
+  })
+}
 
 const htmlEntities = (str: string) => {
   return str
@@ -202,144 +202,140 @@ const htmlEntities = (str: string) => {
     .replace(/&Deg;/g, "°")
     .replace(/&ldquo;/g, "“")
     .replace(/&rdquo;/g, "”")
-    .replace(/&shy;/g, "-");
-};
+    .replace(/&shy;/g, "-")
+}
 
 const shuffleArray = (array: HTMLInputElement[]) => {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
   }
-  return array;
-};
+  return array
+}
 
 const checkAnswer = (event: MouseEvent, answers: HTMLInputElement[]) => {
-  questionCounter += 1;
+  questionCounter += 1
 
-  const answer = event.target as HTMLInputElement;
+  const answer = event.target as HTMLInputElement
 
   if (answer.name === "true") {
-    answer.classList.add("game__answer--correct");
-    correct += 1;
+    answer.classList.add("game__answer--correct")
+    correct += 1
   } else {
-    answer.classList.add("game__answer--incorrect");
-    incorrect += 1;
+    answer.classList.add("game__answer--incorrect")
+    incorrect += 1
     for (const element of answers) {
       if (element.name === "true") {
-        element.classList.add("game__answer--correct");
-        break;
+        element.classList.add("game__answer--correct")
+        break
       }
     }
   }
-  answer.parentElement!.classList.add("game__questions--unclickable");
+  answer.parentElement!.classList.add("game__questions--unclickable")
 
   if (questionCounter === questionAmount) {
     const resultsButton = document.getElementsByClassName(
       "game__navigate--results"
-    );
+    )
 
     for (const button of resultsButton) {
-      button.classList.add("game__navigate--results--show");
+      button.classList.add("game__navigate--results--show")
     }
 
-    endGame();
+    endGame()
   }
-};
+}
 
 const nextQuestion = () => {
-  const gameEl = document.getElementById("game")!;
-  gameEl.children[questionIndex].classList.add("game__questions--hidden");
-  gameEl.children[questionIndex + 1].classList.remove(
-    "game__questions--hidden"
-  );
-  questionIndex += 1;
-};
+  const gameEl = document.getElementById("game")!
+  gameEl.children[questionIndex].classList.add("game__questions--hidden")
+  gameEl.children[questionIndex + 1].classList.remove("game__questions--hidden")
+  questionIndex += 1
+}
 
 const prevQuestion = () => {
-  const gameEl = document.getElementById("game")!;
-  gameEl.children[questionIndex].classList.add("game__questions--hidden");
-  gameEl.children[questionIndex - 1].classList.remove(
-    "game__questions--hidden"
-  );
-  questionIndex -= 1;
-};
+  const gameEl = document.getElementById("game")!
+  gameEl.children[questionIndex].classList.add("game__questions--hidden")
+  gameEl.children[questionIndex - 1].classList.remove("game__questions--hidden")
+  questionIndex -= 1
+}
 
 const showResults = () => {
-  gameEl.classList.add("game--hidden");
-  resultsEl.classList.remove("results--hidden");
-};
+  gameEl.classList.add("game--hidden")
+  resultsEl.classList.remove("results--hidden")
+}
 
 const endGame = () => {
-  resetEl.classList.add("game__reset--hidden");
+  resetEl.classList.add("game__reset--hidden")
 
-  const message = feedback();
-  const resultStatement = document.createElement("p");
-  resultStatement.classList.add("results__statement");
-  resultStatement.innerText = `You got ${correct} out of ${questionAmount} questions right.`;
-  resultStatement.classList.add("results__statement--format");
+  const message = feedback()
+  const resultStatement = document.createElement("p")
+  resultStatement.classList.add("results__statement")
+  resultStatement.innerText = `You got ${correct} out of ${questionAmount} questions right.`
+  resultStatement.classList.add("results__statement--format")
 
-  const resultsFeedback = document.createElement("p");
-  resultsFeedback.classList.add("results__feedback");
-  resultsFeedback.innerText = message;
+  const resultsFeedback = document.createElement("p")
+  resultsFeedback.classList.add("results__feedback")
+  resultsFeedback.innerText = message
 
-  const resultsButton = document.createElement("input");
-  resultsButton.classList.add("results__button");
-  resultsButton.classList.add("game__navigate");
-  resultsButton.type = "button";
-  resultsButton.value = "play again";
-  resultsButton.addEventListener("click", restart);
+  const resultsButton = document.createElement("input")
+  resultsButton.classList.add("results__button")
+  resultsButton.classList.add("game__navigate")
+  resultsButton.type = "button"
+  resultsButton.value = "play again"
+  resultsButton.addEventListener("click", restart)
 
-  const reviewButton = document.createElement("input");
-  reviewButton.classList.add("results__button");
-  reviewButton.classList.add("game__navigate");
-  reviewButton.type = "button";
-  reviewButton.value = "review";
-  reviewButton.addEventListener("click", reviewQuestions);
+  const reviewButton = document.createElement("input")
+  reviewButton.classList.add("results__button")
+  reviewButton.classList.add("game__navigate")
+  reviewButton.type = "button"
+  reviewButton.value = "review"
+  reviewButton.addEventListener("click", reviewQuestions)
 
-  resultsEl.appendChild(resultStatement);
-  resultsEl.appendChild(resultsFeedback);
-  resultsEl.appendChild(reviewButton);
-  resultsEl.appendChild(resultsButton);
-};
+  resultsEl.appendChild(resultStatement)
+  resultsEl.appendChild(resultsFeedback)
+  resultsEl.appendChild(reviewButton)
+  resultsEl.appendChild(resultsButton)
+}
 
 const feedback = () => {
-  const performance = correct / questionAmount;
-  let message;
+  const performance = correct / questionAmount
+  let message
 
   switch (true) {
     case performance >= 0.9:
-      message = "Exceptional!";
-      break;
+      message = "Exceptional!"
+      break
     case performance >= 0.7:
-      message = "Very good! Well done.";
-      break;
+      message = "Very good! Well done."
+      break
     case performance >= 0.5:
-      message = "Not bad! See if you could do better.";
-      break;
+      message = "Not bad! See if you could do better."
+      break
     case performance >= 0.2:
-      message = "hmmm, a bit more studying wouldn't hurt.";
-      break;
+      message = "hmmm, a bit more studying wouldn't hurt."
+      break
     default:
-      message = "Don't bother playing again!";
+      message = "Don't bother playing again!"
   }
-  return message;
-};
+  return message
+}
 
 const restart = () => {
-  resultsEl.innerHTML = "";
-  gameEl.innerHTML = "";
-  gameEl.classList.remove("game--format");
-  form.classList.remove("form--hidden");
-  messageEl.classList.remove("message--hidden");
-};
+  resultsEl.innerHTML = ""
+  gameEl.innerHTML = ""
+  gameEl.classList.remove("game--format")
+  form.classList.remove("form--hidden")
+  messageEl.classList.remove("message--hidden")
+}
 
-resetEl.addEventListener("click", restart);
+resetEl.addEventListener("click", restart)
 
 const reviewQuestions = () => {
-  resultsEl.classList.add("results--hidden");
-  gameEl.classList.remove("game--hidden");
-};
+  resultsEl.classList.add("results--hidden")
+  gameEl.classList.remove("game--hidden")
+}
 
-getCategories();
+getCategories()
